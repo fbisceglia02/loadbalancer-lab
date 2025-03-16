91e27681939af1fe1d1b928df05b77c2b6ae9d6b
fbisceglia02 <abc@company.it>
2025-03-16 18:50:05 +0100

    Fix correlation with a indicies of a referring resource

--- configuration/main.tf
+++ configuration/main.tf
@@ -86,7 +86,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   admin_username                    = var.admin_username # "adminuser"
   admin_password                    = var.admin_password # "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.vm_nic.id,
+    azurerm_network_interface.vm_nic[count.index].id,
   ]
 
   os_disk {
